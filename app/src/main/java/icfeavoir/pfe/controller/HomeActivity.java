c98e0bdc515e834a95cb44c0467144e1989ceef4
Pierre Leroy <pierrotleroy@hotmail.fr>
2018-09-25 17:01:37 +0200

    Modifs

--- app/src/main/java/icfeavoir/pfe/controller/HomeActivity.java
+++ app/src/main/java/icfeavoir/pfe/controller/HomeActivity.java
@@ -1,4 +1,4 @@
-package icfeavoir.pfe;
+package icfeavoir.pfe.controller;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import icfeavoir.pfe.R;
 
 public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
