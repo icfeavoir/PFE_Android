6a9e272f5214fafd279de97dab34e769fd8e4b26
Lucien Christian <lucien.christian@reseau.eseo.fr>
2018-10-16 08:17:36 +0200

    Modification Activities and Layouts

---
+++ app/src/main/java/icfeavoir/pfe/controller/JYINFActivity.java
@@ -0,0 +1,43 @@
+package icfeavoir.pfe.controller;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import java.util.ArrayList;
+
+import icfeavoir.pfe.R;
+import icfeavoir.pfe.model.Project;
+
+public class JYINFActivity extends PFEActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jyinf);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void clickProjectCard(Project project) {
+
+    }
+
+    @Override
+    public void displayData(Object data) {
+
+    }
+}
